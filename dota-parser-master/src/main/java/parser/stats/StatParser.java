70e613bbddfe6db370b054321e528c1d55d98e5c
12yuens2 <sizhe1007@gmail.com>
2018-11-10 17:47:43 +0000

    Add action counts and apm features

--- dota-parser-master/src/main/java/parser/stats/StatParser.java
+++ dota-parser-master/src/main/java/parser/stats/StatParser.java
@@ -1,7 +1,12 @@
-package parser;
+package parser.stats;
 
+import parser.Parser;
 import skadistats.clarity.processor.runner.Context;
+import skadistats.clarity.wire.common.proto.DotaCommonMessages;
+import skadistats.clarity.wire.common.proto.DotaUserMessages;
 import util.ClarityUtil;
+
+import java.util.HashMap;
 
 
 public class StatParser extends Parser {
@@ -9,8 +14,14 @@ public class StatParser extends Parser {
     public static final String PLAYER_RESOURCE = "CDOTA_PlayerResource";
     public static final String TEAM_DATA = "CDOTA_Data";
 
+    public float duration;
+
+    private HashMap<Long, PlayerStats> statsMap;
+
     public StatParser(long filterSteamID) {
         super(filterSteamID);
+
+        statsMap = new HashMap<>();
     }
 
 
@@ -31,6 +42,19 @@ public class StatParser extends Parser {
         System.out.println(String.format(
                 "Kills: %d, Assists: %d, Deaths: %d, Gold: %d, XP: %d, CS: %d, Denies: %d",
                 kills, assists, deaths, totalGold, totalXP, lastHits, denies));
+
+        System.out.println(statsMap.get(filterSteamID).getStats(duration));
+    }
+
+    public void parseUnitOrder(long steamid, DotaUserMessages.CDOTAUserMsg_SpectatorPlayerUnitOrders msg) {
+        if (statsMap.containsKey(steamid)) {
+            PlayerStats stats = statsMap.get(steamid);
+            stats.update(gameTick, msg);
+        } else {
+            PlayerStats stats = new PlayerStats();
+            stats.update(gameTick, msg);
+            statsMap.put(steamid, stats);
+        }
     }
 
     private <T> T getPlayerStat(Context ctx, int id, String entity, String stat) {
